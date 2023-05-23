a0cda3fe49dbb6078759f305a3846cf005618bb1
mikana0918 <shogo.kusuhara@gmail.com>
2023-05-23 16:54:34 +0900

    chore: Set transpilePackages options for pasori

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ["my_number_card_driver", "rc_s380_driver"],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
